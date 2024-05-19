1622d07eb7c4bc8a9ede198ec4da400f014118dd
Alejolora25 <alejandroloratovar@outlook.com>
2024-05-18 20:12:34 -0500

    modificacion controller de usuario

--- controller/userController.js
+++ controller/userController.js
@@ -112,6 +112,7 @@ const loginUser = async (req, res) => {
             if (passwordMatch) {
                 res.json({
                     msg: 'Inicio de sesión exitoso.',
+                    user,
                     tokenSession
                 });
                 return;
