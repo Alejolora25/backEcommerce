6485450946d8b13b81efcc7f6a6faacb04781ca9
Alejolora25 <alejandroloratovar@outlook.com>
2024-05-23 12:55:18 -0500

    se modifica cantidad de imagenes en el card

--- controller/imageController.js
+++ controller/imageController.js
@@ -10,7 +10,7 @@ const getImages = async (req, res) => {
 
 const getImagesPages = async (req, res) => {
     const { page = 1 } = req.query;
-    let size = 3;
+    let size = 8;
     let options = {
         limit: size,
         offset: (page - 1) * (size)
