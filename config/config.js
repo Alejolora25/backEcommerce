7cd22b5b41309ec7684b022c705621fc35c9e835
Edwin <enrojass04@example.com>
2024-06-04 22:13:52 -0500

    Se ajusta tiempo conexión

--- config/config.js
+++ config/config.js
@@ -6,20 +6,47 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000 // 60 segundos de tiempo de espera
+    },
+    pool: {
+      max: 10, // Número máximo de conexiones en el pool
+      min: 0,  // Número mínimo de conexiones en el pool
+      acquire: 60000, // Tiempo máximo en milisegundos que pool intentará obtener una conexión antes de lanzar un error
+      idle: 10000 // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
+    }
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000 // 60 segundos de tiempo de espera
+    },
+    pool: {
+      max: 10, // Número máximo de conexiones en el pool
+      min: 0,  // Número mínimo de conexiones en el pool
+      acquire: 60000, // Tiempo máximo en milisegundos que pool intentará obtener una conexión antes de lanzar un error
+      idle: 10000 // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectOptions: {
+      connectTimeout: 60000 // 60 segundos de tiempo de espera
+    },
+    pool: {
+      max: 10, // Número máximo de conexiones en el pool
+      min: 0,  // Número mínimo de conexiones en el pool
+      acquire: 60000, // Tiempo máximo en milisegundos que pool intentará obtener una conexión antes de lanzar un error
+      idle: 10000 // Tiempo máximo en milisegundos que una conexión puede estar inactiva antes de ser liberada
+    }
   }
 };
