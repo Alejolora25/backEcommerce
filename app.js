b21c6a474d04acc5b9b47a373b42574bc346df17
Alejolora25 <alejandroloratovar@outlook.com>
2024-06-12 12:45:24 -0500

    se agregan conecto local de cors

--- app.js
+++ app.js
@@ -6,19 +6,18 @@ const morgan = require('morgan');
 const cors = require('cors');
 const indexRoutes = require('./routes/index');
 
-
+// Actualiza corsOptions para permitir múltiples orígenes
 const corsOptions = {
-    origin: 'http://localhost:5173/',
-    optionsSuccessStatus: 200 
-}
-
+    origin: ['http://localhost:5173', 'https://front-ecommerce-navy.vercel.app'],
+    optionsSuccessStatus: 200
+};
 
 app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json({ limit: '50mb' })); 
+app.use(cors(corsOptions)); // Aplica CORS globalmente con opciones actualizadas
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use('/api', cors(corsOptions), indexRoutes); // endpoint types
+app.use('/api', indexRoutes); // endpoint types
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
